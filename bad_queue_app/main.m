ca69e3a6e826466aed4904cd9af43384fcec9762
LukeeGD <26163116+LukeZGD@users.noreply.github.com>
2026-03-25 01:51:45 +0800

    Update main.m

--- bad_queue_app/main.m
+++ bad_queue_app/main.m
@@ -64,8 +64,6 @@ int main(int argc, char * argv[]) {
     if (copyfile(get_file_path("resource/tar"), "/bin/tar", NULL, COPYFILE_ALL) != 0) return -1;
     mkdir("/private/var/aquila", 0755);
     if (copyfile(get_file_path("resource/libmis"), "/private/var/aquila/amfi_bypass.dylib", NULL, COPYFILE_ALL) != 0) return -1;
-    if (copyfile(get_file_path("resource/bootstrap.tar"), "/private/var/aquila/bootstrap.tar", NULL, COPYFILE_ALL) != 0) return -1;
-    if (copyfile(get_file_path("resource/truststore.tar"), "/private/var/aquila/truststore.tar", NULL, COPYFILE_ALL) != 0) return -1;
     if (copyfile(get_file_path("resource/aquila"), "/private/var/aquila/aquila", NULL, COPYFILE_ALL) != 0) return -1;
     chmod("/bin/tar", 0755);
     chmod("/private/var/aquila/aquila", 0755);
@@ -74,6 +72,8 @@ int main(int argc, char * argv[]) {
         if (copyfile(get_file_path("resource/launchd.conf"), "/private/etc/launchd.conf", NULL, COPYFILE_ALL) != 0) return -1;
         if (copyfile(get_file_path("resource/installer"), "/private/var/aquila/installer", NULL, COPYFILE_ALL) != 0) return -1;
         if (copyfile(get_file_path("resource/splashscreen.jp2"), "/private/var/aquila/splashscreen.jp2", NULL, COPYFILE_ALL) != 0) return -1;
+        if (copyfile(get_file_path("resource/bootstrap.tar"), "/private/var/aquila/bootstrap.tar", NULL, COPYFILE_ALL) != 0) return -1;
+        if (copyfile(get_file_path("resource/truststore.tar"), "/private/var/aquila/truststore.tar", NULL, COPYFILE_ALL) != 0) return -1;
         chmod("/private/var/aquila/installer", 0755);
     } else {
         if (copyfile(get_file_path("resource/launchd2.conf"), "/private/etc/launchd.conf", NULL, COPYFILE_ALL) != 0) return -1;
